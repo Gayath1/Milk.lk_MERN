10574d17b32d837773dbe9c84d15fe349b876f1c
Gayath1 <chandulagayan@gmail.com>
2021-01-20 18:46:57 +0530

    clean up

--- backend/server.js
+++ backend/server.js
@@ -61,7 +61,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads/')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-//app.use(bodyParser.json());
+
 app.use(cors());
 app.use(cookieParser());
 

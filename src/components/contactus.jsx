2c7ea33c7f3de8c90eb9495d623fa7dcf7d0e834
Gayath1 <chandulagayan@gmail.com>
2021-01-29 15:43:08 +0530

    contact page done

--- src/components/contactus.jsx
+++ src/components/contactus.jsx
@@ -5,8 +5,8 @@ import { Table, Badge } from 'reactstrap';
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
 import Header from '../components/header';
 import { Form, FormGroup, Label, Input, Col, Button,  Alert } from 'reactstrap';
-
-
+import  image from '../images/Allura Walking Around.png';
+import Iframe from 'react-iframe';
 const Store = () => {
     
     return (
@@ -14,7 +14,10 @@ const Store = () => {
       <Header />
       
       
+      <div class="row" style={{width:'100%'}}>
       
+     
+  <div class="col-lg-7 col-lg-offset-2 gauche">
             <div className="contactform">
             <Form  className="contactform1">
             <FormGroup row>
@@ -95,13 +98,21 @@ const Store = () => {
                
            
         </div>
-        
+        </div>
+        <div class="col-lg-4 corps">
         <div className="company">
-            <div className="company details">
-            <a>📍   No: 310, High Level Road, Nawinna, Maharagama.</a>
+            <div className="companydetails">
+            <a>📍   No: 310, High Level Road, Nawinna, Maharagama.</a><br/>
+            <a>📞 Phone: 011 43 10 500 </a><br/>
+            <a>📧 Email: info@arpicosupercentre.com</a><br/>
+            <a>🕐 Support 24/7/365</a><br/>
+            <img  className="imgcontact" src={image}  alt="imageslide"/>
             </div>
         </div>
-        
+        </div>
+      
+        <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.866721365074!2d80.03383615082237!3d7.141406817626173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2fda565c1a6df%3A0xe2443501aa51084f!2sHighland%20Milk%20Bar%20-%20Magalegoda!5e0!3m2!1sen!2slk!4v1611913026083!5m2!1sen!2slk" />
+        </div>
         </div>
 
         

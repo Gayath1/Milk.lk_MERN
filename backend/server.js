521a6796d8554e09c0b102a4cd3d16dbac4d74e9
Gayath1 <chandulagayan@gmail.com>
2021-02-02 17:48:25 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -152,10 +152,10 @@ crudRoutes.route('/delete/:id').delete((req, res) => {
 
 app.use('/all_product', crudRoutes);
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static("build"));
+  app.use(express.static("/build"));
 
   app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"build", "index.html"));
+    res.sendFile(path.join(__dirname,"/build", "index.html"));
   });
 }
 app.listen(PORT, () => {
